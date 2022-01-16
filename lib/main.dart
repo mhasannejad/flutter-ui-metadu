526697a37b40fc2cc143c422154c6152f8390f91
mhasannejad <mngmnt.hasannejad@gmail.com>
2022-01-16 10:44:46 +0330

    almost final commit

--- lib/main.dart
+++ lib/main.dart
@@ -52,83 +52,83 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.transparent,
         body: Container(
             alignment: Alignment.center,
-            child: Column(
-              children: [
-                Container(
-                  padding: EdgeInsets.only(top: 100,bottom: 20),
-                  child: AppTextField(
-                    onChanged: (text) {},
-                    textEditingController: controller,
-                    hintText: 'Search',
-                    icon: LineIcons.search,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(top: 100, bottom: 20),
+                    child: AppTextField(
+                      onChanged: (text) {},
+                      textEditingController: controller,
+                      hintText: 'Search',
+                      icon: LineIcons.search,
+                    ),
                   ),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  margin: EdgeInsets.symmetric(vertical: 10,horizontal: 25),
-                  child: Text(
-                    'Top Selling',
-                    style: TextStyle(fontSize: 25, color: Colors.black87),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                    child: Text(
+                      'Top Selling',
+                      style: TextStyle(fontSize: 25, color: Colors.black87),
+                    ),
                   ),
-                ),
-                Container(
-                  height: 55,
-                  margin: EdgeInsets.symmetric(vertical: 20),
-                  padding: EdgeInsets.symmetric(horizontal: 0),
-                  width: MediaQuery.of(context).size.width,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      ChipItem(emoji: Emojis.monkeyFace, text: 'ArtWorks'),
-                      ChipItem(emoji: Emojis.heartExclamation, text: 'Fonts'),
-                      ChipItem(emoji: Emojis.framedPicture, text: 'Images'),
-                    ],
+                  Container(
+                    height: 55,
+                    margin: EdgeInsets.symmetric(vertical: 20),
+                    padding: EdgeInsets.symmetric(horizontal: 0),
+                    width: MediaQuery.of(context).size.width,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        ChipItem(emoji: Emojis.monkeyFace, text: 'ArtWorks'),
+                        ChipItem(emoji: Emojis.heartExclamation, text: 'Fonts'),
+                        ChipItem(emoji: Emojis.framedPicture, text: 'Images'),
+                      ],
+                    ),
                   ),
-                ),
-                Container(
-                  height: 270,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      NFTItem(
-                        name: 'MekaVerse',
-                        image: 'assets/images/ezgif-2-374e97b74d.png',
-                      ),
-                      NFTItem(
-                        name: 'MetaHorse',
-                        image: 'assets/images/ezgif-2-3a52cad8de.png',
-                      )
-                    ],
+                  Container(
+                    height: 270,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        NFTItem(
+                          name: 'MekaVerse',
+                          image: 'assets/images/ezgif-2-374e97b74d.png',
+                        ),
+                        NFTItem(
+                          name: 'MetaHorse',
+                          image: 'assets/images/ezgif-2-3a52cad8de.png',
+                        )
+                      ],
+                    ),
                   ),
-                ),
-                Container(
-                  height: 40,
-                  width: MediaQuery.of(context).size.width - 50,
-                  margin: EdgeInsets.symmetric(vertical: 10),
-                  child: Row(
-                    children: [
-                      Text(
-                        'TopSellers',
-                        style: TextStyle(fontSize: 25),
-                      ),
-                      Expanded(child: Container()),
-                      Text(
-                        'show more',
-                        style: TextStyle(fontSize: 17),
-                      )
-                    ],
+                  Container(
+                    height: 40,
+                    width: MediaQuery.of(context).size.width - 50,
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    child: Row(
+                      children: [
+                        Text(
+                          'TopSellers',
+                          style: TextStyle(fontSize: 25),
+                        ),
+                        Expanded(child: Container()),
+                        Text(
+                          'show more',
+                          style: TextStyle(fontSize: 17),
+                        )
+                      ],
+                    ),
                   ),
-                ),
-                Container(
-                  height: 50,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      UserItem()
-                    ],
-                  ),
-                )
-              ],
+                  Container(
+                    height: 50,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [UserItem()],
+                    ),
+                  )
+                ],
+              ),
             )),
       ),
     );
--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -1,7 +1,12 @@
 import 'dart:ui';
 
+import 'package:emojis/emojis.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:glass_kit/glass_kit.dart';
+import 'package:metadu_nftshop/components/ChipItem.dart';
+import 'package:metadu_nftshop/components/ntf_item.dart';
 import 'package:metadu_nftshop/hoc/Layout.dart';
 
 class UserPage extends StatefulWidget {
@@ -19,23 +24,97 @@ class _UserPageState extends State<UserPage> {
       backgroundColor: Colors.transparent,
       body: Column(
         children: [
-          new ClipRect(
-            child: new BackdropFilter(
-              filter: new ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
-              child: new Container(
-                width: 450.0,
-                height: 200.0,
-                decoration: new BoxDecoration(
-                    borderRadius: BorderRadius.circular(20.0),
-                    color: Colors.grey.shade200.withOpacity(0.5)),
-                child: Center(
-                    child: Text(
-                  "Censored",
-                  style: TextStyle(fontSize: 30),
-                )),
+          Container(
+            height: MediaQuery.of(context).size.height / 3,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                borderRadius:
+                    BorderRadius.vertical(bottom: Radius.circular(25)),
+                boxShadow: [
+                  BoxShadow(
+                      blurRadius: 10, color: Colors.black87.withOpacity(0.3))
+                ],
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('assets/images/ezgif-2-95d8f7cb6b.png'))),
+          ),
+          InkWell(
+            child: Container(
+              child: Center(
+                child: Text(
+                  'Follow',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.normal),
+                ),
               ),
+              width: MediaQuery.of(context).size.width * 0.7,
+              height: 50,
+              margin: EdgeInsets.symmetric(vertical: 20),
+              decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                        spreadRadius: 4,
+                        blurRadius: 15,
+                        color: Colors.black87.withOpacity(0.2))
+                  ],
+                  gradient: LinearGradient(colors: [
+                    Color(0xffFD6E6A),
+                    Color(0xffFFC600),
+                  ]),
+                  borderRadius: BorderRadius.circular(500)),
             ),
           ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.symmetric(horizontal: 12.5),
+            child: Text(
+              'Mohammad',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.symmetric(
+              horizontal: 12.5,
+              vertical: 15,
+            ),
+            child: Text(
+              'Developer , Designer , Visionary , Gamer',
+              style: TextStyle(fontWeight: FontWeight.normal, fontSize: 18),
+            ),
+          ),
+          Container(
+            height: 270,
+            width: MediaQuery.of(context).size.width,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                NFTItem(
+                    name: 'mekaman',
+                    image:
+                        'assets/images/mekaverse-ethereum-nft-avatars-gID_4.png'),
+                NFTItem(
+                    name: 'mekaman',
+                    image:
+                        'assets/images/mekaverse-ethereum-nft-avatars-gID_4.png')
+              ],
+            ),
+          ),
+          Container(
+            height: 50,
+            margin: EdgeInsets.symmetric(vertical: 15),
+            width: MediaQuery.of(context).size.width,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                ChipItem(emoji: Emojis.monkeyFace, text: 'ArtWorks'),
+                ChipItem(emoji: Emojis.heartExclamation, text: 'Fonts'),
+                ChipItem(emoji: Emojis.framedPicture, text: 'Images'),
+              ],
+            ),
+          )
         ],
       ),
     ));
